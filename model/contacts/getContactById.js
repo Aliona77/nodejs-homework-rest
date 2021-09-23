7ba446b63854e0e5b7b3bfe4e00666e74330afe0
Aliona77 <chabanalona153@gmail.com>
2021-09-23 09:06:49 +0300

    fixed getById

--- model/contacts/getContactById.js
+++ model/contacts/getContactById.js
@@ -2,7 +2,7 @@ const getList = require('./getList')
 
 const getContactById = async (id) => {
   const contacts = await getList()
-  const contact = contacts.find(item => item.id === id)
+  const contact = contacts.find((item) => item.id.toString() === id.toString())
   if (!contact) {
     return null
   }
