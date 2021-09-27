92dea04acf504dc35e5c31102bb200f89a4e91f0
Aliona77 <chabanalona153@gmail.com>
2021-09-28 00:22:16 +0300

    connect to database

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,23 @@
 const app = require('../app')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const PORT = process.env.PORT || 3000
+dotenv.config()
+
+const { DB_HOST, PORT } = process.env
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
 })
+
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Database connection successful')
+  })
+  .catch(error => {
+    console.log(error)
+    process.exit(1)
+  })
