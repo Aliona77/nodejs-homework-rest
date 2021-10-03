13cdf104067729139698a40c3460e844e1b12ca9
Aliona77 <chabanalona153@gmail.com>
2021-10-03 20:20:38 +0300

    get toket

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,6 @@
-const { User } = require('../../models')
 const { BadRequest } = require('http-errors')
+const jwt = require('jsonwebtoken')
+const { User } = require('../../models')
 
 const login = async (req, res) => {
   const { email, password } = req.body
@@ -7,7 +8,15 @@ const login = async (req, res) => {
   if (!user || !user.comparePassword(password)) {
     throw new BadRequest('Invalid email or password')
   }
-  const token = 'dfgfgdgdf.sdfgsdggdfg.dsfgdgdgh'
+
+  const { _id } = user
+  const payload = {
+    _id
+  }
+  const { SECRET_KEY } = process.env
+
+  const token = jwt.sign(payload, SECRET_KEY)
+  await User.findByIdAndUpdate(_id, { token })
   res.json({
     status: 'success',
     code: 200,
