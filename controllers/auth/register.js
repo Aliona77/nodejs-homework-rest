b3f46cd387860738fd8d410f327bafba5ea4ec43
Aliona77 <chabanalona153@gmail.com>
2021-10-03 23:13:50 +0300

    current user done

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,15 +2,20 @@ const { User } = require('../../models')
 const { Conflict } = require('http-errors')
 
 const register = async (req, res) => {
-  const { email, password } = req.body
+  const { email, password, subscription } = req.body
   const user = await User.findOne({ email })
   if (user) {
     throw new Conflict('Email in use')
   }
-  const newUser = new User({ email })
+  const newUser = new User({ email, subscription })
   newUser.setPassword(password)
   await newUser.save()
+
   res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription
+    },
     status: 'success',
     code: 201,
     message: 'Success register'
