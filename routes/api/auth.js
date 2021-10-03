44bbdeda153285dc36df3a4dda83766f45a2bd83
Aliona77 <chabanalona153@gmail.com>
2021-10-03 22:46:57 +0300

    function logout

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const { joiUserSchema } = require('../../models/user')
-const { controllerWrapper, validation } = require('../../middlewares')
+const { controllerWrapper, validation, authenticate } = require('../../middlewares')
 const { auth: ctrl } = require('../../controllers')
 
 router.post('/register', validation(joiUserSchema), controllerWrapper(ctrl.register))
 
 router.post('/login', validation(joiUserSchema), controllerWrapper(ctrl.login))
 
-router.get('/logout', controllerWrapper(ctrl.logout))
+router.get('/logout', authenticate, controllerWrapper(ctrl.logout))
 
 module.exports = router;
