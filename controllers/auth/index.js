93ccdd5ff72867cda1197a10a8c7333590ff22b6
Aliona77 <chabanalona153@gmail.com>
2021-10-11 17:10:24 +0300

    upload avatar

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,12 @@ const register = require('./register')
 const login = require('./login')
 const logout = require('./logout')
 const current = require('./current')
+const addAvatar = require('./addAvatar')
 
 module.exports = {
   register,
   login,
   logout,
-  current
+  current,
+  addAvatar
 }
